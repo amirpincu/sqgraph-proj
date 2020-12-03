0caf8cd7f0f63cfd837ceaeb5c62767d1629b240
amjxart <amir.pincu@gmail.com>
2020-12-03 10:30:56 +0200

    progress

--- resolvers.js
+++ resolvers.js
@@ -1,4 +1,83 @@
+const db = require('./db')
+
+const Client = {
+    branch: ( root, args, context, info ) => {
+        return db.branches.get(root.branch_id);
+    }
+}
+
+const Branch = {
+    clients: ( root, args, context, info ) => {
+        
+        const allClients = db.clients.list();
+        let clientList = [];
+
+        allClients.forEach( (client) => {
+            if (client.branch_id == root.id) {
+                clientList.push(client);
+            }
+        });
+        
+        return clientList;
+    }
+}
+
 const Query = {
-    test: () => 'Test Success, GraphQL server is up & running !!'
+    clients: () => db.clients.list(),
+
+    clientByID: ( root, args, context, info ) => {
+        //args will contain parameter passed in query
+        return db.clients.get(args.id);
+    },
+
+    branches: () => db.branches.list(),
+
+    branchByID: ( root, args, context, info ) => {
+        //args will contain parameter passed in query
+        return db.branches.get(args.id);
+    },
+}
+
+const Mutation = {
+    createClient: (root,args,context,info) => {
+
+        const data = args.data;
+        const id = data.id, name = data.name, adress = data.adress;
+        const balance = data.balance, branch = data.branch_id;
+
+        const clients = db.clients.list();
+        const branches = db.branches.list();
+
+        if ( ( typeof id != "number" ) ) {
+            // return `${typeof id}`;
+            return `Given id must be a number.`;
+        }
+        clients.forEach((currClient) => {
+            if ( currClient.id == id ) {
+                return `A client with the id ${id} already exists. please enter an unregistered id.`;
+            }
+        });
+
+        let branchExistFlag = false;
+        branches.forEach((currBranch) => {
+            if (currBranch.id == branch) {
+                branchExistFlag = true;
+            }
+        });
+        if ( !branchExistFlag ) {
+            return `The branch id does not co-respond to an existing branch in the system.`;
+        }
+
+        db.clients.create({
+            id: id,
+            name: name,
+            adress: adress,
+            balance: balance,
+            branch_id: branch
+        });
+
+        return "Client created succesfully";
+    }
  }
- module.exports = { Query }
+
+module.exports = { Client, Branch, Query, Mutation }
--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const express = require('express');
 const db = require('./db.js');
+const http = require('http');
+const url = require('url');
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 9000;
 const app = express();
 
 const fs = require('fs')
@@ -24,3 +26,44 @@ app.listen(
       `Server started on port ${port}`
    )
 );
+
+/*
+http.createServer(function (req, res) {
+  // const currentUrl = new url.URL( req.url );
+  const current_url = new URL(`http://websitename.com${req.url}`);
+  const parameters = current_url.searchParams;
+  const queryStart = req.url.split('?')[0];
+  const parametersCount = (parameters.toString().match(/=/g) || []).length;
+
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  let retData = undefined;
+  switch (queryStart) {
+    case '/clients':
+
+      if ( parametersCount == 0 ) {
+        retData = db.clients.list();
+      }
+      else if ( parametersCount == 1 && parameters.has("id") ) {
+        retData = db.clients.get( parameters.get("id") * 1 );
+      }
+      else if ( parametersCount == 1 && parameters.has("branch-id") ) {
+        retData = [];
+
+        const branch = parameters.get("branch-id") * 1;
+        const clients = db.clients.list();
+        clients.forEach( (client) => {
+          if ( client.branch_id == branch ) {
+            retData.push( client );
+          }
+        });
+      }
+
+      break;
+    default:
+      retData = "The given request does not exist.";
+  }
+
+  res.end(JSON.stringify(retData));
+ 
+ }).listen(port);
+ */
