4e67c8d5694b8e8d1e220f456d34121f36104f8b
amjxart <amir.pincu@gmail.com>
2020-12-06 17:10:08 +0200

    functional passing of url

--- accesing-server/server.js
+++ accesing-server/server.js
@@ -4,19 +4,23 @@ const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
 http.createServer(function (req, res) {
 
+    const oReq = new XMLHttpRequest();
+    let retVal = undefined;
+
     res.writeHead(200, {'Content-Type': 'text/html'});
-
     console.log(req.url);
-
-    httpReq = XMLHttpRequest();
-    httpReq.onreadystatechange = XMLHttpRequestCompleted;
     
-    httpReq.open("GET", `http://localhost:9000${req.url}`);
-    httpReq.send();
+    oReq.open("GET", `http://localhost:9000${req.url}`);
+    oReq.send();
 
-    httpReq.onreadystatechange = (e) => {
-        res.end(httpReq.responseText);
-        // console.log(http.responseText)
+    let sentBoolCheck = false;
+    oReq.onreadystatechange = (e) => {
+        if ( oReq.status == 200 && !sentBoolCheck ) {
+            sentBoolCheck = true;
+            retVal = oReq.responseText;
+            res.end(retVal);  
+        }
+        console.log(oReq.status);
     }
 
 }).listen(port);
